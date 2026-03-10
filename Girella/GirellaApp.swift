52e6fd95ff0b78c6af45a19b2f8da28b47ce8c17
Adriano Oliviero <adrianoliviero23@gmail.com>
2026-03-10 12:05:13 +0100

    Implemented back gesture

--- Girella/GirellaApp.swift
+++ Girella/GirellaApp.swift
@@ -35,3 +35,16 @@ struct GirellaApp: App {
         .modelContainer(sharedModelContainer)
     }
 }
+
+// Re-enable the interactive pop gesture (swipe to go back)
+// when the native navigation bar back button is hidden.
+extension UINavigationController: UIGestureRecognizerDelegate {
+    override open func viewDidLoad() {
+        super.viewDidLoad()
+        interactivePopGestureRecognizer?.delegate = self
+    }
+
+    public func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
+        return viewControllers.count > 1
+    }
+}
